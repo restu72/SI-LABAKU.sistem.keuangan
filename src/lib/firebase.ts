35b5d0cff0d320f8c6c54a661a28e066f6f5f4c4
Restu <berrysweety567@gmail.com>
2026-04-30 18:39:48 +0700

    Final fix firebase config direct

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,7 +2,15 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-import firebaseConfig from '../../firebase-applet-config.json';
+const firebaseConfig = {
+  apiKey: "AIzaSyCq3vOgspR1zCvEk5bh9pw8bq7Yrzsw0v4",
+  authDomain: "si-labaku-d7687.firebaseapp.com",
+  projectId: "si-labaku-d7687",
+  storageBucket: "si-labaku-d7687.firebasestorage.app",
+  messagingSenderId: "64748198123",
+  appId: "1:64748198123:web:4cbdbc0ded0dabb4446ae",
+  measurementId: "G-MFM4TBTH1J"
+};
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
