e46158b9dff7063858da34381fb64f66aff5ec79
Restu <berrysweety567@gmail.com>
2026-05-01 00:25:19 +0700

    tambah file apk

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.silabaku.app',
+  appName: 'SI LABAKU',
+  webDir: 'dist'
+};
+
+export default config;
